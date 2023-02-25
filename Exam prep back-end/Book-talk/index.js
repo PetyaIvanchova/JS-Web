44d7006d63a63c4243212864a0018563a8bad749
Petya Ivanchova <105751255+PetyaIvanchova@users.noreply.github.com>
2023-02-25 18:21:06 +0200

    Add files via upload

---
+++ Exam prep back-end/Book-talk/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
+const routes = require('./routes');
+const {auth} = require('./middlewares/authMiddleware');
+
+const app = express();
+
+app.engine('hbs', hbs.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.static('public'));
+app.use(cookieParser());
+app.use(auth);
+app.use(routes);
+
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://127.0.0.1:27017/booktalk');
+
+app.listen(5000, () => console.log('Server is running...'));
