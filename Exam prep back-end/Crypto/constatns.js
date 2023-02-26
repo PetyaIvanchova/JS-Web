6d0b41d51ae78d4d7dda9e7aa09c384ad742809e
Petya Ivanchova <105751255+PetyaIvanchova@users.noreply.github.com>
2023-02-26 10:30:20 +0200

    Add files via upload

---
+++ Exam prep back-end/Crypto/constatns.js
@@ -0,0 +1,8 @@
+exports.SECRET ='NZ';
+
+exports.paymentMethodMap = {
+    "crypto-wallet": "Crypto Wallet",
+    "credit-card": "Credit Card",
+    "debit-card": "Debit Card",
+    "paypal": "Paypal"
+}
