f0ac347ade7c16b87e17e21a9edaf7b0646aadcf
Petya Ivanchova <105751255+PetyaIvanchova@users.noreply.github.com>
2023-02-27 19:56:20 +0200

    Create furnitureController.js

---
+++ REST-API/server/controllers/furnitureController.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+
+const furnitureManager = require('../managers/furnitureManager');
+
+router.get('/', async (req, res) => {
+    let ownerId = '';
+    if (req.query.where) {
+        ownerId = req.query.where.split('=')[1].slice(1, -1)
+    }
+
+    const furnitures = await furnitureManager.getAll(ownerId);
+
+    res.json(furnitures);//!!!
+});
+
+router.get('/:furnitureId', async (req, res) => {
+    const furniture = await furnitureManager.getOne(req.params.furnitureId);
+
+    res.json(furniture);//!!!
+});
+
+router.post('/', async (req, res) => {
+    const furnitureData = req.body;
+
+    try {
+        const furniture = await furnitureManager.create(req.user._id, furnitureData);
+
+        res.json({ _id: furniture._id });//!!!
+    } catch (error) {
+        res.status(400).json({ message: error.message, type: 'error' });//!!!
+    }
+});
+
+
+router.put('/:furnitureId', async (req, res) => {
+    const furnitureData = req.body;
+
+    await furnitureManager.update(req.params.furnitureId, furnitureData);
+
+    res.json({ ok: true });//!!!
+});
+
+router.delete('/:furnitureId', async (req, res) => {
+    await furnitureManager.delete(req.params.furnitureId);
+
+    res.json({ ok: true });//!!!
+});
+
+module.exports = router;
